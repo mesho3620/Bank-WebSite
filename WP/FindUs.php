9752af474a41fdc8055634630579daeb7ad5358d
mesho3620 <44414933+mesho3620@users.noreply.github.com>
2019-11-22 20:09:37 +0200

    Update FindUs.php

--- WP/FindUs.php
+++ WP/FindUs.php
@@ -1,90 +1,99 @@
-<!DOCTYPE html>
 
-<?php include ("TopMenu.php");?>
+
+<?php
+include"topmenu.php"
+?>
 
 <html>
+<head></head>
+<style>
 
+.findus{
+	
+	text-align:center;
+	margin:auto;
+	background-color:white;
+	border-style:solid;
+}
+
+.Address{
+	text-align:center;
+	margin:auto;
+
+}
+
+.Phone{
+	text-align:center;
+	margin:auto;
+
+}
+
+.Email{
+    text-align:center;
+	margin:auto;
+
+}
+
+</style>
 <body>
 
-<div id="Find" class="tabcontent"><br>
-<ul>
-	<li><img src="locate.png" width=3.5% height=60px class = "aligning"></li>
-<h1 class="loc">Branches</h1>
-</ul>
-<p> 
 
-<br>
-<b class="loc1">Branch:</b> 10th of Ramadan<br>
-<b class="loc1">City:</b> 10th of Ramadan <br>
-<b class="loc1">Area:</b> 10th of Ramadan <br>
-<b class="loc1">Address:</b> Plot No. 8 Main City Center, Beside El Kafrawy Garden, 10th of Ramadan City. <br>
-<b class="loc1">Working From:</b> 8:30am to 3:00pm <br>
-<b class="loc1">Working Days:</b> Sunday to Thursday <br>
+<div class="findus">
 
-<hr>
+<h1>GET IN TOUCH</h1>
 
-<b class="loc1">Branch:</b> 6th of October <br>
-<b class="loc1">City:</b> 6th of October <br>
-<b class="loc1">Area:</b> Industrial Zone <br>
-<b class="loc1">Address:</b> Plot No. 1/1 Banks Area, 4th Industrial Zone, 6th of October. <br>
-<b class="loc1">Working From:</b> 8:30am to 3:00pm <br>
-<b class="loc1">Working Days:</b> Sunday to Thursday <br>
-
-<hr>
-
-<b class="loc1">Branch:</b> Free Zone Alex. <br>
-<b class="loc1">City:</b> Alexandria <br>
-<b class="loc1">Area:</b> Free Zone <br>
-<b class="loc1">Address:</b> KM 30 Alexandria Cairo Desert Road, Amerya, Alexandria. <br>
-<b class="loc1">Working From:</b> 8:30am to 3:00pm <br>
-<b class="loc1">Working Days:</b> Sunday to Thursday <br>
-
-<hr>
-
-<b class="loc1">Branch:</b> Roushdy <br>
-<b class="loc1">City:</b> Alexandria <br>
-<b class="loc1">Area:</b> Roushdy <br>
-<b class="loc1">Address:</b> 17 Syria St., Roushdy, Alexandria <br>
-<b class="loc1">Working From:</b> 8:30am to 5:00pm <br>
-<b class="loc1">Working Days:</b> Sunday to Thursday <br>
-
-<hr>
-
-<b class="loc1">Branch:</b> Smouha <br>
-<b class="loc1">City:</b> Alexandria <br>
-<b class="loc1">Area:</b> Semouha <br>
-<b class="loc1">Address:</b> 98 Fawzy Moaaz St. & Mohamed Bahaa St. Intersection, El-Lotus Building, Smouha, Alexandria <br>
-<b class="loc1">Working From:</b> 8:30am to 3:00pm <br>
-<b class="loc1">Working Days:</b> Sunday to Thursday <br>
-
-<hr>
-
-<b class="loc1">Branch:</b> Sultan Hussein <br>
-<b class="loc1">City:</b> Alexandria <br>
-<b class="loc1">Area:</b> Sultan Hussein <br>
-<b class="loc1">Address:</b> 55 Sultan Hussein St., Alexandria <br>
-<b class="loc1">Working From:</b> 8:30am to 3:00pm <br>
-<b class="loc1">Working Days:</b> Sunday to Thursday <br>
-
-<hr>
-
-<b class="loc1">Branch:</b> Assuit <br>
-<b class="loc1">City:</b> Assiut <br>
-<b class="loc1">Area:</b> Assuit <br>
-<b class="loc1">Address:</b> 107 Gomhoria St., Beside Petroleum Building, Assuit. <br>
-<b class="loc1">Working From:</b> 8:30am to 3:00pm <br>
-<b class="loc1">Working Days:</b> Sunday to Thursday <br>
-
-<hr>
-
-</p>
-
-<br>
-
+<div>
+<div class="Address">
+<h3>ADDRESS</h3>
+<h5>Weifield Group Contracting</h5>
+<p>146 Yuma Street</p>
+<p>Denver CO 80223</p>
 </div>
 
+<div class="Phone">
+<h3>PHONE</h3>
+<h5>Weifield Group Contracting</h5>
+<p>303.428.2011 phone</p>
+<p>303.202.0466 fascimile</p>
+</div>
+
+<div class="Email">
+<h3>EMAIL</h3>
+<h5>Request for Proposal</h5>
+<p>Info@weifieldgroup.com</p>
+<h5>Electical Service Calls</h5>
+<p>service@weifieldcontracting.com</p>
+</div>
+
+</div>
+</div>
+
+<h2 style="text-decoration:underline;">Message Us</h2>
+
+<form id="MessageForm" action="" onSubmit="" method="post">
+Name <br>
+<input type='text' placeholder="FirstName"> <input type='text' placeholder="Last Name">
+
+<br>
+<br>
+
+Email <br>
+<input type='Email' placeholder="Enter your email address">
+
+<br>
+<br>
+
+Comments <br>
+<textarea rows="4" cols="50"></textarea>
+
+<br>
+<br>
+
+<input type='submit' value="Submit">
+</form>
 
 </body>
 </html>
-
-<?php include ("BottomMenu.php");?>
+<?php
+include"BottomMenu.php"
+?>
