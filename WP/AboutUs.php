d7f24a4b55ba4ab7330b6b067700bfc6e9308cb8
mesho3620 <44414933+mesho3620@users.noreply.github.com>
2019-11-22 20:20:27 +0200

    Update AboutUs.php

--- WP/AboutUs.php
+++ WP/AboutUs.php
@@ -24,61 +24,61 @@
   <hr>
   <h1 style="color:red">Who we are</h1>
   <p>Find out about FNB - its structures, values, community involvement and more.</p> 
-</div>
- 
- <div id="AboutUs_roots">
- <h2 style="color:#CB4335">Our roots</h2>
- <hr>
- <h1 style="color:#F39C12">Humble beginnings</h1>
- <p><b>FNB is the oldest bank in South Africa, and can be traced back to the Eastern Province Bank formed in Grahamstown in 1838. Today, FNB trades as a division of FirstRand Bank Limited. When looking at FNB's history, two things in particular stand out. The first is a story of survival - different circumstances in South Africa have posed many great challenges in our history, all of which FNB has successfully met. This track record provides a strong foundation for our future challenges. The second is a story of people - our history has always been firmly influenced by the needs of the people we serve.
- <br>
-The Acacia tree in our brand logo is a suitable representation of our history. Our roots run deep in South Africa, and we have grown thanks to our commitment to serving the needs of our clients and communities.
-<br>
-A landmark development in FNB's history took place in 1998 when the financial services interests of Rand Merchant Bank Holdings and Anglo American were merged to form FirstRand Limited. In the process, FNB was delisted from the JSE on 22 May 1998 to become a wholly-owned subsidiary of FirstRand, which was listed on the JSE on 25 May 1998. On 30 June 1999, the banking interests of FirstRand formally merged into a single entity to form FirstRand Bank. FNB, WesBank and RMB now trade as divisions of FirstRand Bank.
-</p>
+
+	</div>
+ <div >
+	<h2 style="color:#CB4335">Our roots</h2>
+	<hr>
+	<h1 style="color:#F39C12">Humble beginnings</h1>
+	<p><b>FNB is the oldest bank in South Africa, and can be traced back to the Eastern Province Bank formed in Grahamstown in 1838. Today, FNB trades as a division of FirstRand Bank Limited. When looking at FNB's history, two things in particular stand out. The first is a story of survival - different circumstances in South Africa have posed many great challenges in our history, all of which FNB has successfully met. This track record provides a strong foundation for our future challenges. The second is a story of people - our history has always been firmly influenced by the needs of the people we serve.
+		<br>
+		The Acacia tree in our brand logo is a suitable representation of our history. Our roots run deep in South Africa, and we have grown thanks to our commitment to serving the needs of our clients and communities.
+		<br>
+		A landmark development in FNB's history took place in 1998 when the financial services interests of Rand Merchant Bank Holdings and Anglo American were merged to form FirstRand Limited. In the process, FNB was delisted from the JSE on 22 May 1998 to become a wholly-owned subsidiary of FirstRand, which was listed on the JSE on 25 May 1998. On 30 June 1999, the banking interests of FirstRand formally merged into a single entity to form FirstRand Bank. FNB, WesBank and RMB now trade as divisions of FirstRand Bank.
+	</p>
  </div>
  
-  <div id="AboutUs_FNB_Archives">
- <h2 style="color:#CB4335">FNB Archives</h2>
- <hr>
- <h1 style="color:#F39C12"">On the record</h1>
- <p><b>The FNB Archives Department was established in 1979 to carry out document research and make available to the public the important historical records accumulated by the bank.
- <br>
-The archives initially formed part of the FNB Museum. In May 1997, when the museum closed, it was decided to move the Archives to a new premise. The Archives cater for a wide variety of researchers from many fields of study, such as economic history, genealogy, economics, banking and management studies.
-<br>
-</p>
+  <div >
+	<h2 style="color:#CB4335">FNB Archives</h2>
+	<hr>
+	<h1 style="color:#F39C12"">On the record</h1>
+	<p><b>The FNB Archives Department was established in 1979 to carry out document research and make available to the public the important historical records accumulated by the bank.
+		<br>
+		The archives initially formed part of the FNB Museum. In May 1997, when the museum closed, it was decided to move the Archives to a new premise. The Archives cater for a wide variety of researchers from many fields of study, such as economic history, genealogy, economics, banking and management studies.
+		<br>
+	</p>
   </div>
  
-   <div id="AboutUs_FirstRand_Group">
- <h2 style="color:#CB4335">FirstRand Group</h2>
- <hr>
- <h1 style="color:#F39C12">Of bulls and bears</h1>
- <p><b>Listed on the JSE and the Namibian Stock Exchange, FirstRand Limited is one of the largest financial institutions in South Africa, and provides banking, insurance and investment products and services to retail, commercial, corporate and public sector customers through its portfolio of leading franchises, which include FNB, RMB, WesBank and Ashburton Investments.
- <br>
-FirstRand Bank Limited is a wholly owned subsidiary of FirstRand Limited, and has a number of divisions, including Rand Merchant Bank (the investment bank), FNB (the retail and commercial bank) and WesBank (the instalment finance provider).
-<br>
-</p>
- </div>
+   <div >
+		<h2 style="color:#CB4335">FirstRand Group</h2>
+		<hr>
+		<h1 style="color:#F39C12">Of bulls and bears</h1>
+		<p><b>Listed on the JSE and the Namibian Stock Exchange, FirstRand Limited is one of the largest financial institutions in South Africa, and provides banking, insurance and investment products and services to retail, commercial, corporate and public sector customers through its portfolio of leading franchises, which include FNB, RMB, WesBank and Ashburton Investments.
+			<br>
+			FirstRand Bank Limited is a wholly owned subsidiary of FirstRand Limited, and has a number of divisions, including Rand Merchant Bank (the investment bank), FNB (the retail and commercial bank) and WesBank (the instalment finance provider).
+			<br>
+		</p>
+	</div>
  
-    <div id="AboutUs_The_Associate_Model">
- <h2 style="color:#CB4335">The Associate Model</h2>
- <hr>
- <h1 style="color:#F39C12">Master your own destiny</h1>
- <p><b>The Associate Banker Model will change the face of our business as well as the industry and possibly your life; it has been designed to give the best people in the business the best opportunity to drive revenue for the bank and wealth for themselves.
- <br>
-The owner manager principle will give the incumbent the authority and autonomy to drive a retail branch as their own business.
-<br>
-We are looking for the highest calibre people to take our business forward. Are you one?
-<br>
-<a href="https://www.fnbweb.co.za/associatemodel/" style="text-decoration:none">Visit the Associate Model on our website</a>
-</p>
- </div>
+    <div >
+		<h2 style="color:#CB4335">The Associate Model</h2>
+		<hr>
+		<h1 style="color:#F39C12">Master your own destiny</h1>
+		<p><b>The Associate Banker Model will change the face of our business as well as the industry and possibly your life; it has been designed to give the best people in the business the best opportunity to drive revenue for the bank and wealth for themselves.
+			<br>
+			The owner manager principle will give the incumbent the authority and autonomy to drive a retail branch as their own business.
+			<br>
+			We are looking for the highest calibre people to take our business forward. Are you one?
+			<br>
+			<a href="https://www.fnbweb.co.za/associatemodel/" style="text-decoration:none">Visit the Associate Model on our website</a>
+		</p>
+	</div>
  
-</div>
 
-</div>
 
 </body>
 </html>
 
-
+<?php
+include"BottomMenu.php"
+?>
